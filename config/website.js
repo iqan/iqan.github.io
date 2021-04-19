e943f40b99d098018c15c38bcd379b1d8910d96b
iqans <iqan.shaikh@gmail.com>
2021-04-19 22:59:54 +0100

    .

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 const tailwind = require('../tailwind');
 
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: '/',
 
-  siteTitle: 'Iqan Shaikh', // Navigation and Site Title
-  siteTitleAlt: 'Iqan Shaikh', // Alternative Site title for SEO
-  siteTitleShort: 'Iqan', // short_name for manifest
-  siteHeadline: 'Iqan Shaikh - Consultant - Azure, .NET, Flutter', // Headline for schema.org JSONLD
-  siteUrl: 'https://iqans.com', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logo.png', // Used for SEO and manifest
+  siteTitle: 'Iqan Shaikh',
+  siteTitleAlt: 'Iqan Shaikh',
+  siteTitleShort: 'Iqan',
+  siteHeadline: 'Iqan Shaikh - Sr. Consultant - Azure, .NET, Flutter',
+  siteUrl: 'https://iqans.com',
+  siteLanguage: 'en',
+  siteLogo: '/logo.png',
   siteDescription:
     'Consultation for cloud native applications, website and mobile application development',
-  author: 'Iqan Shaikh', // Author for schema.org JSONLD
+  author: 'Iqan Shaikh',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@iqanshaikh', // Twitter Username
